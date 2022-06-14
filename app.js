5aae30d1a3b1361576ca3470e012f44a54eac82d
brs14shn <brsshn292929@gmail.com>
2022-06-14 10:40:52 +0300

    todoCompleted was added

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ addBtn.addEventListener("click",()=>{
     else{
       myTodos.innerHTML+=`<li class="task"><i class="fa-solid fa-check fa-lg"></i><span class="content">${input.value}</span><i class="fa-solid fa-trash"></i></li>`
       input.value=""
+      todoCompleted()
   
     }
   })
@@ -40,14 +41,14 @@ myTodos.onclick=function(event){
     if(event.target.className=="fa-solid fa-check fa-lg"){
         event.target.nextElementSibling.classList.toggle("line-through");
         event.target.style.color="green"
-        taskCounter()
+        todoCompleted()
       }
       else if (event.target.classList.contains("fa-trash")) {
     
         if(event.target.previousElementSibling.classList.contains("line-through")){
           //console.log(event.target);
         event.target.closest(".task").remove();
-        taskCounter()
+        todoCompleted()
         }
         else{
             alert("Task not completed")
@@ -65,7 +66,23 @@ input.addEventListener('keydown', (event)=>{
   })
 
 
-  
+
+function todoCompleted(){
+let completed=0;
+const todo_status=document.querySelector(".todo_status")
+const totalTasks = document.querySelectorAll(".task").length;
+const span=document.querySelectorAll(".task span").forEach((v)=>{
+  if (v.classList.contains("line-through")){
+   completed++
+  };
+})
+
+todo_status.innerText = `${completed} OUT OF ${totalTasks} TASKS COMPLETED`;
+}
+
+
+
+
 
 
 
